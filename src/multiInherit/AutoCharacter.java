562dd8ce950ab2d47612a96738eb688fcc2af9bc
GEO <georgii.degtyar@gmail.com>
2015-11-18 23:50:34 +0200

    add example multiInherit

---
+++ src/multiInherit/AutoCharacter.java
@@ -0,0 +1,10 @@
+package multiInherit;
+
+/**
+ * Created by GEO on 18.11.15.
+ */
+public class AutoCharacter {
+    public void fight(){
+        System.out.println("AutoCharacter.fight()");
+    }
+}
---
+++ src/multiInherit/Hero.java
@@ -0,0 +1,18 @@
+package multiInherit;
+
+/**
+ * Created by GEO on 18.11.15.
+ */
+public class Hero extends AutoCharacter implements CanFight, CanFly, CanSwim {
+
+    @Override
+    public void fly() {
+        System.out.println("Hero.fly()");
+
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("Hero.swim()");
+    }
+}
